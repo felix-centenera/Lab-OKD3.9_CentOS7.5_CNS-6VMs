c1742fb7e6d11d54c0c6cbc89d2d8278d63ea4e8
felix-centenera <felix.centenera@vass.es>
2020-01-11 14:05:51 +0100

    Resolve vagrant path extra-disks

--- vagrant/bastion/configbastion.sh
+++ vagrant/bastion/configbastion.sh
@@ -6,7 +6,6 @@
 yum update -y
 yum upgrade -y
 yum --enablerepo=extras install epel-release -y
-
 yum -y  install  pyOpenSSL python-pip python-dev sshpass  python-gssapi
 sudo mkdir -p /etc/ansible
 sudo -H pip install --upgrade pip
